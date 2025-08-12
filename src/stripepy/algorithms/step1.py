895ba4f037f048d0e92fa0e128ab65a8884ed5b0
rea1991 <52046472+rea1991@users.noreply.github.com>
2025-08-12 16:28:41 +0200

    Initial implementation of madmax filtering
    Co-authored-by: Andrea Raffo <52046472+rea1991@users.noreply.github.com>
    Co-authored-by: Roberto Rossini <71787608+robomics@users.noreply.github.com>

--- src/stripepy/algorithms/step1.py
+++ src/stripepy/algorithms/step1.py
@@ -7,6 +7,7 @@ from typing import Dict, List, Optional, Tuple
 import numpy as np
 import scipy.sparse as ss
 import structlog
+from numpy.typing import NDArray
 
 from stripepy.data_structures import SparseMatrix
 from stripepy.utils import zero_columns, zero_rows
@@ -50,7 +51,7 @@ def run(
 
     All three matrices have the same shape as the input matrix.
     """
-
+    assert isinstance(matrix, ss.csr_matrix)
     assert genomic_belt > 0
     assert resolution > 0
 
@@ -71,10 +72,23 @@ def run(
 
     roi_matrix_raw = _extract_region_of_interest(matrix, extended_roi)
 
-    logger.bind(step=(1, 1)).info("applying log-transformation")
+    # TODO make madmax params tunable
+    # TODO add flag to skip madmax filtering
+    logger.bind(step=(1, 1)).info("looking for bad bins using the MAD-max filter...")
+    bias = ss.csr_array(_madmax_filter(matrix))
+
+    matrix = matrix.multiply(bias)
+    matrix = matrix.transpose(copy=False)
+    matrix = matrix.multiply(bias.T)
+    matrix = matrix.transpose(copy=False)
+    matrix.eliminate_zeros()  # TODO this may be redundant
+
+    logger.bind(step=(1, 1)).info(f"MAD max filter masked {bias.shape[0] - bias.sum():.0f}/{bias.shape[0]} bins")
+
+    logger.bind(step=(1, 2)).info("applying log-transformation")
     matrix_proc = _log_transform(matrix)
 
-    logger.bind(step=(1, 2)).info("projecting interactions onto [1, 0]")
+    logger.bind(step=(1, 3)).info("projecting interactions onto [1, 0]")
     scaling_factor = matrix_proc.max()
     matrix_proc /= scaling_factor
 
@@ -84,6 +98,40 @@ def run(
         roi_matrix_proc = _log_transform(roi_matrix_raw) / scaling_factor
 
     return matrix_proc, roi_matrix_raw, roi_matrix_proc  # noqa
+
+
+def _madmax_filter(
+    matrix: ss.csr_matrix,
+    min_nnz: int = 10,
+    min_count: int = 0,
+    mad_max: float = 5.0,
+) -> NDArray[float]:
+    """
+    TODO document
+    https://github.com/open2c/cooler/blob/a981796113e3045267b600537ede7617629196ca/src/cooler/_balance.py#L374-L409
+    """
+    assert min_nnz >= 0
+    assert min_count >= 0
+    assert np.isfinite(mad_max)
+    assert mad_max >= 0
+
+    def mad(v: NDArray[float], axis: Optional[int] = None) -> NDArray[float]:
+        return np.median(np.abs(v - np.median(v, axis)), axis)
+
+    marg = matrix.sum(axis=0)
+    if isinstance(marg, np.matrix):
+        marg = marg.A1
+
+    bias = matrix.count_nonzero(axis=0) >= min_nnz
+    bias &= marg >= min_count
+
+    logNzMarg = np.log(marg[marg > 0])
+    med_logNzMarg = np.median(logNzMarg)
+    dev_logNzMarg = mad(logNzMarg)
+    cutoff = np.exp(med_logNzMarg - mad_max * dev_logNzMarg)
+    bias[marg < cutoff] = False
+
+    return bias.astype(float)
 
 
 def _log_transform(matrix: SparseMatrix) -> SparseMatrix:
